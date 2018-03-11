0bc0394ed09477ccfa7141423204a18310d7b5d3
EikiKanaoka <kakakaeee@gmail.com>
2018-03-11 23:32:19 +0900

    layput/navbar , flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  # devise for users 
   devise_for :users
+
+
+  # HOME
   root to: 'home#index'
+  get '/message', to: 'home#message'
+  get 'help', to: 'home#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
