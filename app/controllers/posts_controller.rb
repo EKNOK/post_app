83e434f94d30e0a09697477919a77965dabc0920
EikiKanaoka <kakakaeee@gmail.com>
2018-03-12 20:36:28 +0900

    Post CRUD & user:references

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,50 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :post_correct_user, only: [:destroy]
+
+
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Create Post! Lets try to #{@post.title}"
+      redirect_to @post
+    else
+      flash[:danger] = "Try again !"
+      render @post
+    end
   end
 
   def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:success] = "Post Destroy !"
+    redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def post_correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    if @post.nil?
+      flash[:danger] = "Have no authority to This Post"
+      redirect_to root_url
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length:{maximum: 20}
+  validates :content, presence: true, length:{maximum: 255}
+  validates :user_id, presence: true
+
+
+
+
 end
